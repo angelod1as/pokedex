bbe6d0cffde383fabbd10acf67a76b997c87407b
Angelo Dias <oiangelodias@gmail.com>
2024-10-24 12:17:06 -0300

    increment naked

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -10,8 +10,8 @@ export const Button = ({ className, naked, ...props }: ButtonProps) => {
     <button
       className={clsx(
         className,
-        naked ? "" : "py-1 px-4 font-black",
-        "bg-button-primary rounded border border-text-body hover:bg-button-hover disabled:bg-button-disabled transition-colors duration-100 shadow-md hover:text-primary disabled:text-gray-500 hover:disabled:text-gray-500 "
+        naked ? "" : "py-1 px-4 font-black border border-text-body shadow-md",
+        "bg-button-primary rounded  hover:bg-button-hover disabled:bg-button-disabled transition-colors duration-100  hover:text-primary disabled:text-gray-500 hover:disabled:text-gray-500 flex flex-col justify-center items-center"
       )}
       {...props}
     />
